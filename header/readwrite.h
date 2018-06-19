2450b17e51606f0a7ef7c22e6125a4e2c3224793
LLran <katsudonn@163.com>
2018-06-19 16:03:46 +0800

    readn

--- header/readwrite.h
+++ header/readwrite.h
@@ -34,6 +34,40 @@ Writen(int fd, void *ptr, size_t nbytes)
 		err_sys("writen error");
 }
 
+ssize_t						/* Read "n" bytes from a descriptor. */
+readn(int fd, void *vptr, size_t n)
+{
+	size_t	nleft;
+	ssize_t	nread;
+	char	*ptr;
+
+	ptr = (char*)vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nread = read(fd, ptr, nleft)) < 0) {
+			if (errno == EINTR)
+				nread = 0;		/* and call read() again */
+			else
+				return(-1);
+		} else if (nread == 0)
+			break;				/* EOF */
+
+		nleft -= nread;
+		ptr   += nread;
+	}
+	return(n - nleft);		/* return >= 0 */
+}
+/* end readn */
+
+ssize_t
+Readn(int fd, void *ptr, size_t nbytes)
+{
+	ssize_t		n;
+	if ( (n = readn(fd, ptr, nbytes)) < 0)
+		err_sys("readn error");
+	return(n);
+}
+
 
 static int	read_cnt;
 static char	*read_ptr;
@@ -91,5 +125,7 @@ Readline(int fd, void *ptr, size_t maxlen)
 		err_sys("readline error");
 	return(n);
 }
+
+
 
 #endif
